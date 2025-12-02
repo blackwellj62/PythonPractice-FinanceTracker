f4b44dbfaecb902dc68a1ca2032d101bb09b24b6
Joey Blackwell <blackwellj6241@gmail.com>
2025-12-02 12:07:57 -0600

    list practice

--- intro.py
+++ intro.py
@@ -2,3 +2,15 @@ name = "Joey"
 hobby = "skateboarding"
 
 print("My name is", name, "and I enjoy", hobby, "!"  )
+
+cities = ["Nashville", "Murfreesboro", "Tullahoma"]
+
+print(cities[2])
+
+cities.append("Estill Springs")
+cities.append("Lewisburg")
+cities.append("Lexington")
+
+
+print(cities)
+
